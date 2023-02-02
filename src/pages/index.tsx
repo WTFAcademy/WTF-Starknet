8f0592bdbf84d5216aa86fedac2a246257e56774
PCDotFan <i@g4.cx>
2023-02-03 00:57:50 +0800

    feat: 增加 @starknet/react 示例

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,30 +3,29 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import {GlobalContext} from "@site/src/contexts/GlobalContext";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import { useAccount } from "@starknet-react/core";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-    const {siteConfig} = useDocusaurusContext();
-    const {test} = useContext(GlobalContext);
-
-    return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
-            <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <div className={styles.buttons}>
-                    <Link
-                        className="button button--secondary button--lg"
-                        to="/docs/intro">
-                        StarkNet Tutorials
-                    </Link>
-                </div>
-            </div>
-        </header>
-    );
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            StarkNet Tutorials
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default function Home(): JSX.Element {
