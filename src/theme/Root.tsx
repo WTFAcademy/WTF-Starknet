25b757e5e91a37d6756d16099acabe26b1b9833e
chenchongqiang <chongqiangchen@gmail.com>
2023-02-04 00:57:33 +0800

    feat: add toast and rename sbt page

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,21 +1,23 @@
 import React, {useState} from 'react';
-import { StarknetConfig, InjectedConnector } from "@starknet-react/core";
+import {StarknetConfig, InjectedConnector} from "@starknet-react/core";
 import GlobalContext from "@site/src/contexts/GlobalContext";
+import { Toaster } from "react-hot-toast";
 
 // Default implementation, that you can customize
 export default function Root({children}) {
-  const connectors = [
-    new InjectedConnector({ options: { id: "braavos" } }),
-    new InjectedConnector({ options: { id: "argentX" } }),
-  ];
+    const connectors = [
+        new InjectedConnector({options: {id: "braavos"}}),
+        new InjectedConnector({options: {id: "argentX"}}),
+    ];
 
-  const [uid, setUid] = useState(undefined);
+    const [uid, setUid] = useState(undefined);
 
-  return (
-      <StarknetConfig connectors={connectors}>
-        <GlobalContext.Provider value={{uid, setUid}}>
-          {children}
-        </GlobalContext.Provider>
-      </StarknetConfig>
-  );
+    return (
+        <StarknetConfig connectors={connectors}>
+            <GlobalContext.Provider value={{uid, setUid}}>
+                {children}
+                <Toaster position="top-center" />
+            </GlobalContext.Provider>
+        </StarknetConfig>
+    );
 }
