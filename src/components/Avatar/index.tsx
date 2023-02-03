0e8d67c749def5e0d742d266e3c3f843b74021de
chenchongqiang <chongqiangchen@gmail.com>
2023-02-03 22:17:57 +0800

    refactor: refactor connect wallet style and add api

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Jazzicon from "react-jazzicon";
+import {useAccount} from "@starknet-react/core";
+
+interface AvatarProps extends React.PropsWithChildren<any> {
+    address?: string;
+    diameter?: number;
+}
+
+const Avatar = (props: AvatarProps) => {
+    const { address, diameter = 30, ...other } = props;
+    const { address: accountAddress } = useAccount();
+    console.log(address);
+    const newAddress = address || accountAddress || '';
+    const seed = parseInt(newAddress.slice(2, 10), 16);
+
+    return (
+        <Jazzicon diameter={diameter} seed={seed} {...other} />
+    )
+}
+
+export default Avatar;
