b3d74900af43a470abcffe890bb0efd1e2ff51ff
chenchongqiang <chongqiangchen@gmail.com>
2023-02-04 17:03:32 +0800

    refactor: refactor doc logic

--- src/constants/course.ts
+++ src/constants/course.ts
@@ -1,3 +1,11 @@
 
 
 export const CourseId = '783590a2-a3e1-11ed-bd3b-fe813f55b524';
+
+export const Lesson_1 = 'c8acff1a-a3e2-11ed-8418-fe813f55b524';
+export const Lesson_2 = '14f4a6fc-a3e3-11ed-a02f-fe813f55b524';
+export const Lesson_3 = '372e5b64-a3e3-11ed-ab57-fe813f55b524';
+export const Lesson_4 = '5a7aed12-a3e3-11ed-ac42-fe813f55b524';
+export const Lesson_5 = '7a05a83e-a3e3-11ed-aa9f-fe813f55b524';
+
+
