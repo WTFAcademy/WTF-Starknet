23c2c65969d1da133a346e27513734d4d57ad0f4
Li, Amazing Ang <d0u.gie@hotmail.com>
2023-02-12 03:47:05 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -102,6 +102,9 @@ const config = {
             label: "Tutorial",
           },
           { to: "/blog", label: "Blog", position: "left" },
+          // {
+          //   type: 'localeDropdown',
+          // },  
           {
             type: "custom-profile",
             position: "right",
