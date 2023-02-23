d81899e0d59dc8e632b61039f4f3209ff0a02850
Li, Amazing Ang <d0u.gie@hotmail.com>
2023-02-23 23:08:17 +0800

    disable {obtain certificate} button

--- src/components/QuizDashboard/index.tsx
+++ src/components/QuizDashboard/index.tsx
@@ -105,7 +105,7 @@ export default function QuizDashboard(props) {
                 </ul>
                 <p style={{marginTop: 16, marginBottom: 16}}>When you have completed all the courses, please click the
                     button below to get the certificate.</p>
-                <Button type="primary" onClick={() => history.push('/sbt')}>
+                <Button disabled type="primary" onClick={() => history.push('/sbt')}>
                     Obtain Certificate
                     <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8 1L13 6M13 6L8 11M13 6H1" stroke="white" stroke-linecap="round"
