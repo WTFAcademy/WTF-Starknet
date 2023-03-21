0f7d6a6337612fe7f142d110be233a0e5e4f0307
Adrian <adrianstoj@gmail.com>
2023-03-21 22:19:48 +0100

    fix: edit this page button link to repository

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,14 +67,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/WTFAcademy/WTF-Starknet/edit/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/WTFAcademy/WTF-Starknet/edit/main/",
         },
         theme: {
           customCss: [require.resolve("./src/css/custom.css")],
